bec3223978140ee76484e55d6c227fe3eac2a530
okade <okade.y@progfast.jp>
2013-11-29 09:31:19 +0900

    PEMファイル作成スクリプト
    IISからエクスポートしたPXFファイルから秘密鍵とサーバー証明書を取り出し、
    指定した中間CA証明書とクロスルート用CA証明書と合わせたPEMファイルを作成する

---
+++ trad-b/create_pemfile.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+## === PATH config ===
+CURRENT_PATH=$(pwd)
+DOMAIN_NAME=pf.ws-g.jp
+
+## === PEM file check ===
+if [ -f ./${DOMAIN_NAME}.pem ]; then
+        echo "Exist ${DOMAIN_NAME}.pem file!!  Overwrite OK?(yes/no)"
+
+        read proceed
+        if test $proceed != "yes"; then
+                echo "Abort!!"
+                exit
+        fi
+fi
+
+if [ -f ./${DOMAIN_NAME}.key ]; then
+        echo "Exist ${DOMAIN_NAME}.key file!!  Overwrite OK?(yes/no)"
+
+        read proceed
+        if test $proceed != "yes"; then
+                echo "Abort!!"
+                exit
+        fi
+fi
+
+if [ -f ./${DOMAIN_NAME}.crt ]; then
+        echo "Exist ${DOMAIN_NAME}.crt file!!  Overwrite OK?(yes/no)"
+
+        read proceed
+        if test $proceed != "yes"; then
+                echo "Abort!!"
+                exit
+        fi
+fi
+
+
+## === SET FILEs ===
+
+echo "PFX file name ?"
+read pfx_filename 
+#pfx_filename="pf.ws-g.jp.pfx"
+if [ ! -f ./${pfx_filename} ]; then
+	echo "No exists PFX file!!"
+	echo "Abort!!"
+	exit
+fi
+
+echo "Intermediate CA file name ?"
+read intermediate_ca_filename
+#intermediate_ca_filename="CybertrustJapanPublicCAG2.cer"
+if [ ! -f ./${intermediate_ca_filename} ]; then
+        echo "No exists Intermediate CA file!!"
+	echo "Abort!!"
+	exit
+fi
+
+echo "CrossRoot CA file name ?"
+read crossroot_ca_filename
+#crossroot_ca_filename="BaltimoreCyberTrustRoot.cer"
+if [ ! -f ./${crossroot_ca_filename} ]; then
+        echo "No exist Intermediate CA file!!  Proceed?(yes/no)"
+
+	read proceed
+	if test $proceed != "yes"; then
+		echo "Abort!!"
+		exit
+	fi
+fi
+
+
+## === PEX -> KEY, CER ===
+echo ""
+echo "============================================="
+echo "create KEY and CRT from  PFX file"
+echo "============================================="
+echo ""
+
+openssl pkcs12 -in ${pfx_filename} -out ${DOMAIN_NAME}.key -nocerts -nodes
+if test $? != 0; then
+        echo "Failed!!"
+        exit
+fi
+
+openssl pkcs12 -in ${pfx_filename} -out ${DOMAIN_NAME}.crt -clcerts -nokeys
+if test $? != 0; then
+        echo "Failed!!"
+        exit
+fi
+
+echo ""
+echo "============================================="
+echo "create PEM file from KEY, CRT, Intermediate CA and Crossroot CA"
+echo "============================================="
+echo ""
+
+
+cat ${DOMAIN_NAME}.key > ${DOMAIN_NAME}.pem
+cat ${DOMAIN_NAME}.crt >> ${DOMAIN_NAME}.pem
+cat ${intermediate_ca_filename} >> ${DOMAIN_NAME}.pem
+
+if [ -f ./${crossroot_ca_filename} ]; then
+	cat ${crossroot_ca_filename} >> ${DOMAIN_NAME}.pem
+fi
+
+cp ${DOMAIN_NAME}.pem ${DOMAIN_NAME}.pem.bak
+tr -d '\r' < ${DOMAIN_NAME}.pem.bak > ${DOMAIN_NAME}.pem
+rm -f ${DOMAIN_NAME}.pem.bak
+
+echo "finish!!"
